3c25e8c5aa84f5d6a3a8d5aa9ac1b3affd865df8
Kostadin Seymenski <seimenski@icloud.com>
2021-03-22 21:43:50 +0200

    Implemented Create POST Functionality

--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -1,24 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../../model/Product");
-// const jwt = require("jsonwebtoken");
-// const User = require("../../model/User");
+const jwt = require("jsonwebtoken");
+const User = require("../../model/User");
 
 router.get("/", (req, res) => {
   Product.find({}).then((products) => {
     res.status(200).json(products);
   });
-  
 });
 
-
-router.post('/create', (req, res) => {
+router.post("/create", (req, res) => {
   const { title, description, imageUrl, price, category } = req.body;
+  const token = req.headers.authorization.split(" ")[1];
+  const decoded = jwt.verify(token, "yoursecret");
+  const userId = decoded._id;
   
-  if (title === '' || description === '' || imageUrl === "" || price === "" || category === "") {
+
+  if (
+    title === "" ||
+    description === "" ||
+    imageUrl === "" ||
+    price === "" ||
+    category === ""
+  ) {
     return res.status(400).json({
-      msg: 'Please fill all the inputs.'
-    })
+      msg: "Please fill all the inputs.",
+    });
   }
 
   const newProduct = new Product({
@@ -26,9 +34,19 @@ router.post('/create', (req, res) => {
     description,
     imageUrl,
     price,
-    category
+    category,
   });
 
+  User.findById(userId).then((data) => {
+    data.myProducts.push(newProduct);
+    data.save().then(() => {
+      console.log(data.myProducts);
+      return res.status(201).json({
+        success: true,
+        msg: "Product Created Successfully.",
+      });
+    });
+  });
 });
 
 module.exports = router;
